76a6b6a2bbb017a295c0278e4d2f4e493d976f21
Steve Sun <>
2022-07-10 18:04:10 +0800

    update

--- src/containers/MainCard/MainCard.jsx
+++ src/containers/MainCard/MainCard.jsx
@@ -1,16 +1,77 @@
 import React, { Component } from 'react';
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Text, Link } from '@chakra-ui/react';
+import {
+  CopyIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  InfoIcon,
+  LinkIcon,
+} from '@chakra-ui/icons';
 
 class MainCardComponent extends Component {
   state = {
-    name: 'dev',
+    name: 'card',
+    idea: '',
+    author: '',
+    intro: '',
+    curator: ' ',
+  };
+  constructor(props) {
+    super(props);
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    const response = await fetch('https://q24.io/api/v1/ideas:random_one');
+    const data = await response.json();
+    this.setState({
+      idea: data.idea,
+      url: data.url,
+      author: data.author,
+      intro: data.intro,
+      curator: data.curator,
+    });
   };
 
   render() {
     return (
-      <Box w='620px' h='420px' borderRadius='19px' bg='#e6e7ee' boxShadow='15px 15px 30px #c4c4ca, -15px -15px 30px #ffffff' textAlign='center'>
-        <p>Hello, {this.state.name}!</p>
-      </Box>
+      <Flex
+        flexDirection="column"
+        w="620px"
+        h="420px"
+        padding="30px"
+        borderRadius="19px"
+        bg="#e6e7ee"
+        boxShadow="15px 15px 30px #c4c4ca, -15px -15px 30px #ffffff"
+      >
+        <Flex flexDirection="row" h="10%">
+          <Flex flexDirection="row" w="50%" justifyContent="flex-start">
+            <ChevronLeftIcon marginRight="10px" />
+            <ChevronRightIcon />
+          </Flex>
+          <Flex flexDirection="row" w="50%" justifyContent="flex-end">
+            {/* <DownloadIcon marginRight="10px" /> */}
+            <CopyIcon />
+          </Flex>
+        </Flex>
+        <Box w="100%" h="70%" textAlign="left">
+          <Text fontSize="lg" noOfLines={5}>
+            {this.state.idea}
+            <Link marginLeft="10px" href={this.state.url}>
+              <LinkIcon />
+            </Link>
+          </Text>
+        </Box>
+        <Box w="100%" h="7%" textAlign="right">
+          <Text fontSize="md">{this.state.author}</Text>
+        </Box>
+
+        <Box w="100%" marginTop="10px" textAlign="right" alignItems="center">
+          <Text fontSize="xs" color="grey">
+            本内容由 <Text as="u">{this.state.curator}</Text> 提供
+          </Text>
+        </Box>
+      </Flex>
     );
   }
 }
--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,33 +1,24 @@
-import * as React from 'react'
-import { ChakraProvider, Box, HStack, Flex } from '@chakra-ui/react'
+import * as React from 'react';
+import { ChakraProvider, Box, HStack, Flex, Select } from '@chakra-ui/react';
 
 import MainCardComponent from '../../containers/MainCard/MainCard';
-import InfoComponent from '../../containers/Info/Info';
-
 
 const Newtab = () => {
   return (
     <ChakraProvider>
-      <Flex flexDirection='column' h='100vh' bg='#e6e7ee'>
-        <HStack w='100vw' flex='1'>
-          <Box w="25vw">1</Box>
-          <Box w='25vw'>
+      <Flex flexDirection="column" h="100vh" bg="#e6e7ee">
+        <Flex flexDirection="row" justifyContent="flex-end" h="80px">
+          <Select w="100px" placeholder="Auto" margin="auto 50px">
+            <option value="light"> Light </option>
+            <option value="dark">Dark</option>
+          </Select>
+        </Flex>
+        <HStack w="100vw" flex="1" justifyContent="center">
+          <Box>
             <MainCardComponent></MainCardComponent>
           </Box>
-          <Box w='25vw'>3</Box>
-          <Box w='25vw'>4</Box>
         </HStack>
-        <Box w="100vw" h='50px' textAlign='center'>
-          <InfoComponent></InfoComponent>
-        </Box>
-
       </Flex>
-      {/* <MainCardComponent></MainCardComponent> */}
-      {/* <footer>
-        <p>制作：<a href="https://twitter.com/way2steve" target="_blank" rel="noreferrer">Steve</a> & <a
-          href="https://twitter.com/fm100" target="_blank" rel="noreferrer">Bob</a></p>
-        <p>支持：<a href="https://club.q24.io/">灵感买家俱乐部会员</a></p>
-      </footer> */}
     </ChakraProvider>
   );
 };
